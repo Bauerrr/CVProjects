d21b6b67639b9e933f347546d10bdb695ad102c7
Bauerrr <33778131+Bauerrr@users.noreply.github.com>
2023-02-27 21:13:06 +0100

    Models for Feddit

---
+++ Django/Feddit/main/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    user = models.ForeignKey('User', on_delete=models.SET_NULL)
+    likes = models.IntegerField()
+    image = models.ImageField()
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    kcal = models.IntegerField()
+    proteins = models.IntegerField()
+    fats = models.IntegerField()
+    carbs = models.IntegerField()
+    hashtags = models.JSONField()
+
+
+class User(models.Model):
+    nickname = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=256)
+    
+
+class Comment(models.Model):
+    user = models.ForeignKey('User', on_delete=models.SET_NULL)
+    likes = models.IntegerField()
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
+
+
+class Hashtag(models.Model):
+    name = models.CharField(max_length=15, unique=True)
